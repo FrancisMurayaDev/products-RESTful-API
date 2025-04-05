1d5efa9f3676b52b927609473986bfc77431f89f
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-05 22:17:22 +0300

    get all products

--- src/controllers.ts
+++ src/controllers.ts
@@ -5,3 +5,10 @@ export const getAllProducts = async (_req: Request, res: Response) => {
   const products = await prisma.product.findMany();
   res.json(products);
 };
+
+export const getProduct = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) return res.status(404).json({ message: 'No product was found.' });
+  res.json(product);
+};
