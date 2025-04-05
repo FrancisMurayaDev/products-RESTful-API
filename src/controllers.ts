f93750a731af370d35efab48e84766ff09c0bb8b
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-05 22:15:47 +0300

    get all products

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,2 +1,7 @@
 import { Request, Response } from 'express';
 import prisma from './prisma';
+
+export const getAllProducts = async (_req: Request, res: Response) => {
+  const products = await prisma.product.findMany();
+  res.json(products);
+};
