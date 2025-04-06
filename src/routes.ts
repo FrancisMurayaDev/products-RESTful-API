6ba246590c11c9feb660c049daff4a2cdb8167eb
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-06 08:23:20 +0300

    define the routes

---
+++ src/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import {
+  getAllProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+  getOfferProducts,
+} from './controllers';
+
+const router = express.Router();
+
+router.get('/products', getAllProducts);
+router.get('/products/offers', getOfferProducts);
+router.get('/products/:id', getProduct);
+router.post('/products', createProduct);
+router.put('/products/:id', updateProduct);
+router.delete('/products/:id', deleteProduct);
+
+export default router;
