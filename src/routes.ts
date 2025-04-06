e3a967fc37582ca751a2c7d3fceb84c6c2aaffdf
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-06 08:55:23 +0300

    typescript more errors

--- src/routes.ts
+++ src/routes.ts
@@ -12,7 +12,7 @@ const router = express.Router();
 
 router.get('/products', getAllProducts);
 router.get('/products/offers', getOfferProducts);
-// router.get('/products/:id', getProduct);
+router.get('/products/:id', getProduct);
 router.post('/products', createProduct);
 router.put('/products/:id', updateProduct);
 router.delete('/products/:id', deleteProduct);
