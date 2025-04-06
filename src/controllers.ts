2d84853aa6d83b5c56a22bffa64e934d4984b011
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-06 08:49:50 +0300

    revert changes

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,28 +1,16 @@
 import { Request, Response } from 'express';
 import { PrismaClient } from '@prisma/client';
-
 const prisma = new PrismaClient();
-
-type ParamsWithId = {
-  id: string;
-};
 
 export const getAllProducts = async (_req: Request, res: Response) => {
   const products = await prisma.product.findMany();
   res.json(products);
 };
 
-export const getProduct = async (req: Request<ParamsWithId>, res: Response) => {
-  const id = parseInt(req.params.id, 10);
-  if (isNaN(id)) {
-    return res.status(400).json({ message: 'Invalid product ID' });
-  }
-
+export const getProduct = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
   const product = await prisma.product.findUnique({ where: { id } });
-  if (!product) {
-    return res.status(404).json({ message: 'No product was found.' });
-  }
-
+  if (!product) return res.status(404).json({ message: 'No product was found.' });
   res.json(product);
 };
 
@@ -34,12 +22,8 @@ export const createProduct = async (req: Request, res: Response) => {
   res.status(201).json(product);
 };
 
-export const updateProduct = async (req: Request<ParamsWithId>, res: Response) => {
-  const id = parseInt(req.params.id, 10);
-  if (isNaN(id)) {
-    return res.status(400).json({ message: 'Invalid product ID' });
-  }
-
+export const updateProduct = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
   const { productTitle, productDescription, unitsLeft, pricePerUnit, isOnOffer } = req.body;
   const product = await prisma.product.update({
     where: { id },
@@ -48,12 +32,8 @@ export const updateProduct = async (req: Request<ParamsWithId>, res: Response) =
   res.json(product);
 };
 
-export const deleteProduct = async (req: Request<ParamsWithId>, res: Response) => {
-  const id = parseInt(req.params.id, 10);
-  if (isNaN(id)) {
-    return res.status(400).json({ message: 'Invalid product ID' });
-  }
-
+export const deleteProduct = async (req: Request, res: Response) => {
+  const id = Number(req.params.id);
   await prisma.product.delete({ where: { id } });
   res.status(204).send();
 };
