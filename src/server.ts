9f3dcbaedea442d372e4ff80fec3710f15ce4801
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-06 08:25:40 +0300

    set up express listening port server

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import router from './routes';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use('/api', router);
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}/api`);
+});
